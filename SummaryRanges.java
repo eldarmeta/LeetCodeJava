5dce3e73f151b752bc1c663fa267faaff5d34b23
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-07-31 22:08:41 -0400

    Add summary ranges

---
+++ SummaryRanges.java
@@ -0,0 +1,16 @@
+// LeetCode: Summary Ranges
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+            int end = nums[i];
+            if (start == end) result.add(String.valueOf(start));
+            else result.add(start + "->" + end);
+        }
+        return result;
+    }
+}
