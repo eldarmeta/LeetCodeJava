55ae31ba8df64c23719f21c66bb231637e7b9890
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-08-01 22:47:25 -0400

    Add leetcode LongSub without repeating

---
+++ LongestSubstringWithoutRepeating.java
@@ -0,0 +1,30 @@
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeating {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int left = 0, right = 0, maxLen = 0;
+        HashSet<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            if (!set.contains(c)) {
+                set.add(c);
+                maxLen = Math.max(maxLen, right - left + 1);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String input = "abcabcbb";
+        int result = lengthOfLongestSubstring(input);
+        System.out.println("Length of longest substring without repeating characters: " + result);
+    }
+}
